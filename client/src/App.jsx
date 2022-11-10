622b3b345b9b1752d57daed803ed4a11de76a0c1
Yisehak Abreham <yworku@uplandsoftware.com>
2022-11-10 10:55:23 +0300

    add markers to map

---
+++ client/src/App.jsx
@@ -0,0 +1,144 @@
+import { useEffect, useRef } from 'react';
+import { fromLonLat, transform } from 'ol/proj';
+import { Point } from 'ol/geom';
+import { Feature } from 'ol';
+import Map from 'ol/Map.js';
+import View from 'ol/View.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+import Text from 'ol/style/Text';
+import pin from './assets/pointer.png';
+import flag from './assets/flag.png';
+import logo from './assets/logo.png';
+import './App.css';
+import Fill from 'ol/style/Fill';
+import Stroke from 'ol/style/Stroke';
+
+const mockMarkers = [
+	{ price: 12, location: [0.123, 53.005] },
+	{ price: 13, location: [0.129, 53.004] },
+	{ price: 14, location: [0.11, 53.003] },
+];
+
+function App() {
+	const container = useRef();
+	const map = useRef();
+	// centers to london by default
+	const center = transform([0.123, 53], 'EPSG:4326', 'EPSG:3857');
+
+	/*
+	*
+	Render the map
+	*
+	*/
+	useEffect(() => {
+		if (!container.current || map.current) return;
+
+		const currentLocation = new Feature(new Point(fromLonLat([0.123, 53])));
+		currentLocation.setStyle(
+			new Style({
+				image: new Icon({
+					src: pin,
+					scale: 0.1,
+				}),
+			})
+		);
+
+		const labelStyle = new Style({
+			image: new Icon({
+				src: flag,
+				scale: 0.08,
+			}),
+			text: new Text({
+				font: '16px Calibri,sans-serif',
+				overflow: true,
+				fill: new Fill({
+					color: '#fff',
+				}),
+				stroke: new Stroke({
+					color: '#000',
+					width: 15,
+				}),
+				text: '$ 26.23',
+				offsetY: 40,
+			}),
+		});
+
+		var markers = new VectorLayer({
+			source: new VectorSource(),
+			style: function (feature) {
+				labelStyle.getText().setText(feature.get('name'));
+				return [labelStyle];
+			},
+		});
+		// markers.getSource().addFeature(currentLocation);
+		mockMarkers.map((m) => {
+			const a = new Feature({
+				geometry: new Point(fromLonLat(m.location)),
+				name: `$ ${m.price}`,
+			});
+			markers.getSource().addFeature(a);
+		});
+
+		map.current = new Map({
+			layers: [new TileLayer({ source: new OSM() }), markers],
+			view: new View({
+				zoom: 15,
+				center,
+			}),
+			controls: [],
+			target: container.current,
+		});
+	}, [container.current]);
+
+	return (
+		<>
+			<div className='logo-wrapper p-5'>
+				<img src={logo} style={{ width: 150 }} />
+			</div>
+			<div ref={container} className='map'></div>
+			<div className='map-controls p-5'>
+				<div className='me-3'>
+					<div class='input-group input-group-lg'>
+						<input
+							class='form-control shadow border-dark'
+							placeholder='Enter postal code'
+						/>
+					</div>
+				</div>
+				<div>
+					<div class='input-group'>
+						<div class='btn-group btn-group-lg bg-white shadow' role='group'>
+							<input type='radio' class='btn-check' checked />
+							<label class='btn btn-outline-dark'>Diesel</label>
+							<input type='radio' class='btn-check' />
+							<label class='btn btn-outline-dark'>Petrol</label>
+							<input type='radio' class='btn-check' />
+							<label class='btn btn-outline-dark'>Electric</label>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='navigation p-5'>
+				<button
+					type='button'
+					class='btn btn-lg btn-light mb-3 shadow-sm border-dark'
+				>
+					<i class='bi bi-info-lg'></i>
+				</button>
+				<button
+					type='button'
+					class='btn btn-lg btn-light mb-3 shadow-sm border-dark'
+				>
+					<i class='bi bi-people-fill'></i>
+				</button>
+			</div>
+		</>
+	);
+}
+
+export default App;
